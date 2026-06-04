e130f62577c53adfb7aae042662d8b944151b9de
LucioAlec <lucioalecpro@gmail.com>
2026-06-04 14:51:26 -0300

    Add nested comments routes and controller actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+  before_action :set_post, only: [:index, :new, :create]
+  def index
+    @comments = @post.comments
+  end
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.new(comments_params)
+
+    if @comment.save
+      redirect_to post_comments_path(@post), notice: "OK"
+    else
+      flash.now[:alert] = "OOPS!"
+      render :new, :unprocessable_entity
+    end
+  end
+    
+
+
+private 
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comments_params
+    params.require(:comment).permit(:description)
+  end
+
+end
