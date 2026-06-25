b7fd7852e186af00cd3c437698f3a98292aad207
LucioAlec <lucioalecpro@gmail.com>
2026-06-25 12:16:03 -0300

    Remove index action, add edit and destroy action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: [ :index, :new, :create ]
-  def index
-    @comments = @post.comments
-  end
+  before_action :set_post
+  before_action :set_comment,  only: [ :edit, :update, :destroy ]
 
   def new
     @comment = @post.comments.new
@@ -12,18 +10,38 @@ class CommentsController < ApplicationController
     @comment = @post.comments.new(comments_params)
 
     if @comment.save
-      redirect_to post_comments_path(@post), notice: "OK"
+      redirect_to post_path(@post), notice: "OK"
     else
       flash.now[:alert] = "OOPS!"
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit ; end
+
+  def update
+    if @comment.update(comments_params)
+      redirect_to @post, notice: "Comment updated successfully"
+    else
+      flash.now[:alert] = "Post not updated"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+      redirect_to @post, notice: "Comment successfully deleted!"
+  end
 
 
 private
+
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comments_params
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
   resources :posts do
-    resources :comments, only: [ :index, :new, :create ]
+    resources :comments, except: [ :index ]
   end
 end
