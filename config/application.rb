8caa34319dd17a5c4db5f4a902d7254233e771d7
LucioAlec <lucioalecpro@gmail.com>
2026-08-07 23:48:33 -0300

    Update app configuration for Rails 8.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Blog1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
 
     config.time_zone = "Brasilia"
 
